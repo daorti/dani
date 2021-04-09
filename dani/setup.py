c32a6fa36053e97822026564b2f63051d372a521
daorti <daneys90@hotmail.com>
2021-04-09 12:43:59 +0200

    licese bien

--- dani/setup.py
+++ dani/setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'daorti',                   # Type in your name
   author_email = 'daniel@thebridgeschool.es',      # Type in your E-Mail
   url = 'https://github.com/daorti/dani',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/daorti/dani/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['hacker', 'guapo', 'flipas'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
@@ -19,7 +19,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
